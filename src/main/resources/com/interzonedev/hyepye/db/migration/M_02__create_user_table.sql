45eac2a63c6da37c4d4f5e6e4e034b7b823f8ccf
Mark Markarian <mark@interzoendev.com>
2014-06-16 16:47:58 -0700

    Added database migrations to create the time_updated updating function, the hp_user table and the verb table.

---
+++ src/main/resources/com/interzonedev/hyepye/db/migration/M_02__create_user_table.sql
@@ -0,0 +1,32 @@
+-- Create the hp_user table, primary key sequence, update timestamp set trigger and constraints.
+
+DROP TABLE IF EXISTS hp_user;
+
+DROP SEQUENCE IF EXISTS hp_user_seq;
+
+DROP TRIGGER IF EXISTS t_set_hp_user_time_updated ON hp_user;
+
+CREATE SEQUENCE hp_user_seq;
+
+CREATE TABLE hp_user (
+    hp_user_id INTEGER NOT NULL PRIMARY KEY DEFAULT nextval('hp_user_seq'),
+    username VARCHAR(128) NOT NULL,
+    password_hash CHAR(64) DEFAULT NULL,
+    password_seed CHAR(10) DEFAULT NULL,
+    email VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) DEFAULT NULL,
+    last_name VARCHAR(255) DEFAULT NULL,
+    active BOOLEAN DEFAULT TRUE,
+    admin BOOLEAN DEFAULT FALSE,
+    time_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    time_updated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TRIGGER t_set_hp_user_time_updated
+    BEFORE UPDATE
+    ON hp_user
+    FOR EACH ROW
+    EXECUTE PROCEDURE fn_update_timestamp();
+
+ALTER TABLE hp_user ADD CONSTRAINT u_hp_user_username UNIQUE (username);
+ALTER TABLE hp_user ADD CONSTRAINT u_hp_user_email UNIQUE (email);
---
+++ src/main/resources/com/interzonedev/hyepye/db/migration/M_03__create_verb_table.sql
@@ -0,0 +1,33 @@
+-- Create the verb table, primary key sequence, update timestamp set trigger, constraints and indicies.
+
+DROP TABLE IF EXISTS verb;
+
+DROP SEQUENCE IF EXISTS verb_seq;
+
+CREATE SEQUENCE verb_seq;
+
+CREATE TABLE verb (
+    verb_id INTEGER NOT NULL PRIMARY KEY DEFAULT nextval('verb_seq'),
+    armenian VARCHAR(255) COLLATE "hy_AM.UTF-8" NOT NULL,
+    english VARCHAR(255) NOT NULL,
+    conjugation_group VARCHAR(5) NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    time_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    time_updated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_by INTEGER NOT NULL,
+    modified_by INTEGER NOT NULL,
+    CONSTRAINT fk_verb_created_by_hp_user_hp_user_id FOREIGN KEY (created_by) REFERENCES hp_user (hp_user_id),
+    CONSTRAINT fk_verb_modified_by_hp_user_hp_user_id FOREIGN KEY (modified_by) REFERENCES hp_user (hp_user_id)
+);
+
+CREATE TRIGGER t_set_verb_time_updated
+    BEFORE UPDATE
+    ON verb
+    FOR EACH ROW
+    EXECUTE PROCEDURE fn_update_timestamp();
+
+ALTER TABLE verb ADD CONSTRAINT u_verb_armenian UNIQUE (armenian);
+
+CREATE INDEX idx_verb_english ON verb (english);
+CREATE INDEX idx_verb_conjugation_group ON verb (conjugation_group);
+CREATE INDEX idx_verb_status ON verb (status);
