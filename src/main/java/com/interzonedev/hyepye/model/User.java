a25c76975e024b1d16cae98b1477adf78fb1737b
Mark Markarian <mark@interzoendev.com>
2014-06-23 09:29:40 -0700

    Altered the User model to adopt the builder pattern for creating new User instances.

--- src/main/java/com/interzonedev/hyepye/model/User.java
+++ src/main/java/com/interzonedev/hyepye/model/User.java
@@ -11,6 +11,11 @@ import org.hibernate.validator.constraints.NotEmpty;
 
 import com.google.common.base.Objects;
 
+/**
+ * Domain model class for users.
+ * 
+ * @author mmarkarian
+ */
 public class User implements Serializable {
 
     private static final long serialVersionUID = -8164945372635497639L;
@@ -51,19 +56,43 @@ public class User implements Serializable {
 
     private final Date timeUpdated;
 
-    public User(Long id, String username, String passwordHash, String passwordSeed, String email, String firstName,
-            String lastName, String role, boolean active, Date timeCreated, Date timeUpdated) {
-        this.id = id;
-        this.username = username;
-        this.passwordHash = passwordHash;
-        this.passwordSeed = passwordSeed;
-        this.email = email;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.role = role;
-        this.active = active;
-        this.timeCreated = timeCreated;
-        this.timeUpdated = timeUpdated;
+    /**
+     * Creates a new {@link User} from the values set on the specified {@link Builder}.
+     * 
+     * @param builder The {@link Builder} that holds the values for the {@link User} to create.
+     */
+    private User(Builder builder) {
+        this.id = builder.id;
+        this.username = builder.username;
+        this.passwordHash = builder.passwordHash;
+        this.passwordSeed = builder.passwordSeed;
+        this.email = builder.email;
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.role = builder.role;
+        this.active = builder.active;
+        this.timeCreated = builder.timeCreated;
+        this.timeUpdated = builder.timeUpdated;
+    }
+
+    /**
+     * Gets a {@link Builder} with default initial values.
+     * 
+     * @return Returns a {@link Builder} with default initial values.
+     */
+    public static Builder newBuilder() {
+        return new Builder();
+    }
+
+    /**
+     * Gets a {@link Builder} with initial values set from the specified template {@link User}
+     * 
+     * @param template A {@link User} from which to get the initial values for the new {@link Builder}.
+     * 
+     * @return Returns a {@link Builder} with initial values set from the specified template {@link User}
+     */
+    public static Builder newBuilder(User template) {
+        return new Builder(template);
     }
 
     public Long getId() {
@@ -146,6 +175,124 @@ public class User implements Serializable {
                 .add("passwordSeed", getPasswordSeed()).add("email", getEmail()).add("firstName", getFirstName())
                 .add("lastName", getLastName()).add("role", getRole()).add("active", isActive())
                 .add("timeCreated", getTimeCreated()).add("timeUpdated", getTimeUpdated()).toString();
+    }
+
+    /**
+     * Mutable builder for creating instances of {@link User}. Allows for the individual setting of properties.
+     * 
+     * @author mmarkarian
+     */
+    public static class Builder {
+
+        private Long id;
+
+        private String username;
+
+        private String passwordHash;
+
+        private String passwordSeed;
+
+        private String email;
+
+        private String firstName;
+
+        private String lastName;
+
+        private String role;
+
+        private boolean active;
+
+        private Date timeCreated;
+
+        private Date timeUpdated;
+
+        /**
+         * Default constructor. Allows for building a {@link User} starting with default values.
+         */
+        private Builder() {
+        }
+
+        /**
+         * Allows for building a {@link User} starting with initial values set from the specified template {@link User}.
+         * 
+         * @param template A {@link User} from which to get the initial values for building a new {@link User}.
+         */
+        private Builder(User template) {
+            this.id = template.getId();
+            this.username = template.getUsername();
+            this.passwordHash = template.getPasswordHash();
+            this.passwordSeed = template.getPasswordSeed();
+            this.email = template.getEmail();
+            this.firstName = template.getFirstName();
+            this.lastName = template.getLastName();
+            this.role = template.getRole();
+            this.active = template.isActive();
+            this.timeCreated = template.getTimeCreated();
+            this.timeUpdated = template.getTimeUpdated();
+        }
+
+        /**
+         * Creates a new {@link User} from the values set on this {@link Builder}.
+         */
+        public User build() {
+            return new User(this);
+        }
+
+        public Builder setId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setUsername(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public Builder setPasswordHash(String passwordHash) {
+            this.passwordHash = passwordHash;
+            return this;
+        }
+
+        public Builder setPasswordSeed(String passwordSeed) {
+            this.passwordSeed = passwordSeed;
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder setFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder setRole(String role) {
+            this.role = role;
+            return this;
+        }
+
+        public Builder setActive(boolean active) {
+            this.active = active;
+            return this;
+        }
+
+        public Builder setTimeCreated(Date timeCreated) {
+            this.timeCreated = timeCreated;
+            return this;
+        }
+
+        public Builder setTimeUpdated(Date timeUpdated) {
+            this.timeUpdated = timeUpdated;
+            return this;
+        }
+
     }
 
 }
