632667921f029fbc31ad58c0f52514891b96fb77
Mark Markarian <mark@interzoendev.com>
2014-06-29 17:15:25 -0700

    Added a migration to add the vocabulary table.

---
+++ src/main/resources/com/interzonedev/hyepye/db/migration/M_06__create_vocabulary_table.sql
@@ -0,0 +1,36 @@
+-- Create the vocabulary table, primary key sequence, update timestamp set trigger, constraints and indicies.
+
+DROP TABLE IF EXISTS vocabulary;
+
+DROP SEQUENCE IF EXISTS vocabulary_seq;
+
+CREATE SEQUENCE vocabulary_seq;
+
+CREATE TABLE vocabulary (
+    vocabulary_id INTEGER NOT NULL PRIMARY KEY DEFAULT nextval('vocabulary_seq'),
+    armenian VARCHAR(255) COLLATE "hy_AM.UTF-8" NOT NULL,
+    english VARCHAR(255) NOT NULL,
+    conjugation_group VARCHAR(5) NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    vocabulary_type VARCHAR(50) NOT NULL,
+    time_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    time_updated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_by INTEGER NOT NULL,
+    modified_by INTEGER NOT NULL,
+    CONSTRAINT fk_vocabulary_created_by_hp_user_hp_user_id FOREIGN KEY (created_by) REFERENCES hp_user (hp_user_id),
+    CONSTRAINT fk_vocabulary_modified_by_hp_user_hp_user_id FOREIGN KEY (modified_by) REFERENCES hp_user (hp_user_id)
+);
+
+DROP TRIGGER IF EXISTS t_set_vocabulary_time_updated ON vocabulary;
+
+CREATE TRIGGER t_set_vocabulary_time_updated
+    BEFORE UPDATE
+    ON vocabulary
+    FOR EACH ROW
+    EXECUTE PROCEDURE fn_update_timestamp();
+
+ALTER TABLE vocabulary ADD CONSTRAINT u_vocabulary_armenian UNIQUE (armenian);
+
+CREATE INDEX idx_vocabulary_english ON vocabulary (english);
+CREATE INDEX idx_vocabulary_status ON vocabulary (status);
+CREATE INDEX idx_vocabulary_vocabulary_type ON vocabulary (vocabulary_type);
