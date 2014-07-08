d3bee0010ad39d2cd165b6722c68ee86e0f6f9e4
Mark Markarian <mark@interzoendev.com>
2014-07-07 23:10:52 -0700

    Added a test suite for the user command integration tests.

--- src/test/java/com/interzonedev/hyepye/HyePyeTestSuite.java
+++ src/test/java/com/interzonedev/hyepye/HyePyeTestSuite.java
@@ -6,6 +6,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 import com.interzonedev.hyepye.model.ModelTestSuite;
 import com.interzonedev.hyepye.service.command.CommandTestSuite;
+import com.interzonedev.hyepye.service.command.user.UserCommandTestSuite;
 
 /**
  * This is a convenience class for running all the tests for the entire Hye Pye application at once by an IDE. This
@@ -14,6 +15,6 @@ import com.interzonedev.hyepye.service.command.CommandTestSuite;
  * @author mmarkarian
  */
 @RunWith(Suite.class)
-@SuiteClasses({ ModelTestSuite.class, CommandTestSuite.class })
+@SuiteClasses({ ModelTestSuite.class, CommandTestSuite.class, UserCommandTestSuite.class })
 public class HyePyeTestSuite {
 }
