4fe6e50cadc7d8ee4a75d531defe970ef4326edd
Mark Markarian <mark@interzoendev.com>
2014-07-14 15:57:55 -0700

    Got rid of the extraneous GetUserByNameCommandIT in CommandTestSuite.

--- src/test/java/com/interzonedev/hyepye/service/command/CommandTestSuite.java
+++ src/test/java/com/interzonedev/hyepye/service/command/CommandTestSuite.java
@@ -4,8 +4,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import com.interzonedev.hyepye.service.command.user.GetUserByNameCommandIT;
-
 /**
  * This is a convenience class for running all the tests for the {@link com.interzonedev.hyepye.service.command} package
  * at once by an IDE. This should not be run by the Maven build.
@@ -13,6 +11,6 @@ import com.interzonedev.hyepye.service.command.user.GetUserByNameCommandIT;
  * @author mmarkarian
  */
 @RunWith(Suite.class)
-@SuiteClasses({ GetUserByNameCommandIT.class, HyePyeResponseTest.class })
+@SuiteClasses({ HyePyeResponseTest.class })
 public class CommandTestSuite {
 }
