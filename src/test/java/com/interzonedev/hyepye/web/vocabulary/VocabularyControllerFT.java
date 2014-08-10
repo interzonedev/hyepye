922ab6e87d00ba5a823ffc6cb1179b86548ff4b2
Mark Markarian <mark@interzoendev.com>
2014-08-09 23:31:14 -0700

    Deleted VocabularyControllerFT.

--- src/test/java/com/interzonedev/hyepye/web/vocabulary/VocabularyControllerFT.java
+++
@@ -1,9 +0,0 @@
-package com.interzonedev.hyepye.web.vocabulary;
-
-public class VocabularyControllerFT {
-
-    public VocabularyControllerFT() {
-        // TODO Auto-generated constructor stub
-    }
-
-}
