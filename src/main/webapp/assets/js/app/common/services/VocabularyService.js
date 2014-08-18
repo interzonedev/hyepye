78d350590896c86fe26b115a2fd0c957cf07349e
Mark Markarian <mark@interzoendev.com>
2014-08-18 14:17:41 -0700

    Refactored the getVocabularyTypes and getVocabularyProperties methods in VocabularyService in the front end to cache results and return promises with the success case returning the expected results directly.

--- src/main/webapp/assets/js/app/common/services/VocabularyService.js
+++ src/main/webapp/assets/js/app/common/services/VocabularyService.js
@@ -8,32 +8,48 @@
     /**
      * Defines a service for sending requests to the Vocabulary endpoints.
      */
-    services.service("VocabularyService", function ($http, ServiceUtils) {
+    services.service("VocabularyService", function ($q, $http, ServiceUtils) {
 
-        var vocabularyTypesUrl, vocabularyPropertiesUrl, vocabularyTypes, vocabularyProperties;
-
-        vocabularyTypesUrl =   "/vocabulary/types";
-        
-        vocabularyPropertiesUrl =   "/vocabulary/properties";
+        var vocabularyTypes, vocabularyProperties, handleGetVocabularyTypesError, handleGetVocabularyPropertiesError;
 
         this.getVocabularyTypes = function() {
-            
-            if (!vocabularyTypes) {
-                vocabularyTypes = $http.get(vocabularyTypesUrl);
+            if (vocabularyTypes) {
+                var deferred;
+                deferred = $q.defer();
+                deferred.resolve(vocabularyTypes);
+                return deferred.promise;
+            } else {
+                return $http.get("/vocabulary/types").then(function success(response) {
+                    if (response && response.data && response.data.vocabularyTypes) {
+                        vocabularyTypes = response.data.vocabularyTypes;
+                        return response.data.vocabularyTypes;
+                    } else {
+                        return handleGetStatusesError(response);
+                    }
+                }, function error(response) {
+                    return handleGetStatusesError(response);
+                });
             }
-
-            return vocabularyTypes;
-
         };
 
         this.getVocabularyProperties = function() {
-            
-            if (!vocabularyTypes) {
-                vocabularyTypes = $http.get(vocabularyTypesUrl);
+            if (vocabularyProperties) {
+                var deferred;
+                deferred = $q.defer();
+                deferred.resolve(vocabularyProperties);
+                return deferred.promise;
+            } else {
+                return $http.get("/vocabulary/properties").then(function success(response) {
+                    if (response && response.data && response.data.vocabularyProperties) {
+                        vocabularyProperties = response.data.vocabularyProperties;
+                        return response.data.vocabularyProperties;
+                    } else {
+                        return handleGetStatusesError(response);
+                    }
+                }, function error(response) {
+                    return handleGetStatusesError(response);
+                });
             }
-
-            return vocabularyTypes;
-
         };
 
         this.search = function(params) {
@@ -46,6 +62,31 @@
             
         };
         
+        
+        handleGetVocabularyTypesError = function(response) {
+            var message;
+
+            message = "Error retrieving vocabulary types";
+            $log.error("VocabularyService: getVocabularyProperties - " + message);
+            $rootScope.$broadcast("alert", {
+                "msg": message
+            });
+
+            return $q.reject(response);
+        };
+
+        handleGetVocabularyPropertiesError = function(response) {
+            var message;
+
+            message = "Error retrieving vocabulary properties";
+            $log.error("VocabularyService: getVocabularyProperties - " + message);
+            $rootScope.$broadcast("alert", {
+                "msg": message
+            });
+
+            return $q.reject(response);
+        };
+
     });
 
 }());
