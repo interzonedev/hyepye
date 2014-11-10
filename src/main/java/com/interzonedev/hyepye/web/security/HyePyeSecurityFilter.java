2bf5b54a8e87228802d2d5cd045c1b6bbb52b0c5
Mark Markarian <mark@interzoendev.com>
2014-11-09 22:18:53 -0800

    Added a security filter to redirect /loginForm and /login to HTTPS in production.

---
+++ src/main/java/com/interzonedev/hyepye/web/security/HyePyeSecurityFilter.java
@@ -0,0 +1,80 @@
+package com.interzonedev.hyepye.web.security;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.inject.Named;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.interzonedev.herokusupport.environment.Environment;
+
+@Named("hyepye-securityFilter")
+public class HyePyeSecurityFilter implements Filter {
+
+    private static final Logger log = LoggerFactory.getLogger(HyePyeSecurityFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+            ServletException {
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        StringBuilder logMessage = new StringBuilder();
+
+        logMessage.append(StringUtils.repeat("*", 10)).append(" ").append(" (").append(httpRequest.hashCode())
+                .append("): ").append(httpRequest.getMethod()).append(" ").append(httpRequest.getScheme()).append(" ")
+                .append(httpRequest.getServerName()).append(" ").append(httpRequest.getLocalPort()).append(" ")
+                .append(httpRequest.getRemotePort()).append(" ").append(httpRequest.getLocalAddr()).append(" ")
+                .append(httpRequest.getRemoteAddr()).append(" ").append(httpRequest.getRequestURI()).append(" ")
+                .append(StringUtils.repeat("*", 10));
+
+        log.debug("doFilter: Start - " + logMessage);
+
+        boolean needsRedirect = Environment.PRODUCTION.equals(Environment.getCurrentEnvironment())
+                && (!"https".equals(httpRequest.getScheme()));
+
+        if (needsRedirect) {
+            // The request is HTTP, needs a redirect.
+            try {
+                URI redirectURI = new URI("https", httpRequest.getServerName(), httpRequest.getRequestURI(), null);
+                String redirectURL = redirectURI.toString();
+                log.debug("doFilter: Redirecting to " + redirectURL);
+                httpResponse.sendRedirect(redirectURL);
+                return;
+            } catch (URISyntaxException e) {
+                String errorMessage = "Error creating redirect URL";
+                log.error("doFilter: " + errorMessage, e);
+                throw new ServletException(errorMessage, e);
+            }
+        } else {
+            // The request is HTTPS, continue.
+            log.debug("doFilter: Continuing filter chain");
+            chain.doFilter(httpRequest, response);
+        }
+
+        log.debug("doFilter: End - " + logMessage);
+
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+}
